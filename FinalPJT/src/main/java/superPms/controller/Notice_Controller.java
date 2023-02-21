cae1e7e83f3e79c13b99e5bc233035a7a7d95ef2
YooMinWoo <alsn0527@naver.com>
2023-02-21 23:48:43 +0900

    경로 수정

--- FinalPJT/src/main/java/superPms/controller/Notice_Controller.java
+++ FinalPJT/src/main/java/superPms/controller/Notice_Controller.java
@@ -5,30 +5,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Notice_Controller {
-//	http://localhost:2030/FinalPJT/goNoticePM.do
+//	http://localhost:2030/FinalPJT/goNotice.do		==> user
+//	http://localhost:2030/FinalPJT/goNoticePM.do	==> admin
 	@RequestMapping("/goNotice.do")
 	public String goNotice() {
-		return "WEB-INF\\ymwView\\notice.jsp";
+		return "WEB-INF\\minwooView\\notice.jsp";
 	}
 	
 	@RequestMapping("/goNoticePM.do")
 	public String goNoticePM() {
-		return "WEB-INF\\ymwView\\notice_pm.jsp";
+		return "WEB-INF\\minwooView\\notice_pm.jsp";
 	}
 	
 //	http://localhost:2030/FinalPJT/goCreateNotice.do
 	@RequestMapping("/goCreateNotice.do")
 	public String goCreateNotice() {
-		return "WEB-INF\\ymwView\\notice_create_pm.jsp";
+		return "WEB-INF\\minwooView\\notice_create_pm.jsp";
 	}
 	
 	@RequestMapping("/goNoticeDetail.do")
 	public String goNoticeDetail() {
-		return "WEB-INF\\ymwView\\notice_detail.jsp";
+		return "WEB-INF\\minwooView\\notice_detail.jsp";
 	}
 	
 	@RequestMapping("/goNoticeDetailPM.do")
 	public String goNoticeDetailPM() {
-		return "WEB-INF\\ymwView\\notice_detail_pm.jsp";
+		return "WEB-INF\\minwooView\\notice_detail_pm.jsp";
 	}
 }
