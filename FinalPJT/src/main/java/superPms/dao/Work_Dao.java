52ab90252d204e6f791277023b9552b0678d3fcb
Jord_N <Jord_N@LAPTOP-HMF00UAB>
2023-02-22 18:34:47 +0900

    업무

--- FinalPJT/src/main/java/superPms/dao/Work_Dao.java
+++ FinalPJT/src/main/java/superPms/dao/Work_Dao.java
@@ -6,7 +6,7 @@ import superPms.vo.Work;
 
 public interface Work_Dao {
 	public List<Work> getWorkList();
-	public Work getWorkDetail();
-	public void insWork(Work ins);
-	public void delWork(Work del);
+//	public Work getWorkDetail();
+//	public void insWork(Work ins);
+//	public void delWork(Work del);
 }
